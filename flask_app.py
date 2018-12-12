c04a5f72d2faa4ab23598991d9a94954f19666dd
Daniela Isabel Travieso <dtravie@gmail.com>
2018-12-12 13:25:48 -0500

    working update and insert

--- flask_app.py
+++ flask_app.py
@@ -11,12 +11,14 @@ def sql_database():
     from functions.sqlquery import sql_query, sql_query3
     flowers_table = sql_query('''SELECT * FROM Flowers''')
     sightings_results = sql_query3('''SELECT * FROM SIGHTINGS WHERE NAME='Draperia' ORDER BY SIGHTED DESC''', 10)
+    sightings_table = sql_query('''SELECT * FROM Sightings''')
 
     return render_template('sqldatabase.html', 
     	sightings_results=sightings_results,
+    	sightings_table=sightings_table,
     	flowers_table=flowers_table)
 
-# TODO: INSERT ON SIGHTINGS TABLE 
+# TODO: REFINE QUERY TO BE MORE ROBUST
 @app.route('/insert',methods = ['POST', 'GET']) #this is when user submits an insert
 def sql_datainsert():
     from functions.sqlquery import sql_edit_insert, sql_query
@@ -28,17 +30,20 @@ def sql_datainsert():
         # TODO: SET THIS EQUAL TO SOMETHING MAYBE IT'LL WORK
         sql_edit_insert(''' INSERT OR REPLACE INTO Sightings (name,person,location,sighted) VALUES (?,?,?,?) ''', (name,person,location,sighted) )
     flowers_table = sql_query('''SELECT * FROM Flowers''')
+    sightings_table = sql_query('''SELECT * FROM Sightings''')
+
     return render_template('sqldatabase.html', 
+    	sightings_table=sightings_table,
     	flowers_table=flowers_table) 
 
 @app.route('/query_edit',methods = ['POST', 'GET']) #this is when user clicks edit link
 def sql_editlink():
     from functions.sqlquery import sql_query, sql_query2
     if request.method == 'GET':
-        egenus = request.args.get('genus')
-        especies = request.args.get('species')
-        ecomname = request.args.get('comname')
-        eresults = sql_query2(''' SELECT * FROM Flowers WHERE genus = ? AND species = ? ''',(egenus,ecomname))
+    	egenus = request.args['egenus']
+    	especies = request.args['especies']
+    	ecomname = request.args['ecomname']
+    	eresults = sql_query2(''' SELECT * FROM Flowers WHERE genus = ? AND species = ? ''',(egenus,especies))
     flowers_table = sql_query('''SELECT * FROM Flowers''')
     return render_template('sqldatabase.html', 
     	eresults=eresults,
@@ -46,7 +51,7 @@ def sql_editlink():
 
 @app.route('/edit',methods = ['POST', 'GET']) #this is when user submits an edit
 def sql_dataedit():
-    from functions.sqlquery import sql_edit_insert, sql_query
+    from functions.sqlquery import sql_edit_insert, sql_query, sql_query3
     if request.method == 'POST':
     	old_genus = request.form['old_genus']
     	old_species = request.form['old_species']
@@ -54,9 +59,13 @@ def sql_dataedit():
     	genus = request.form['genus']
     	species = request.form['species']
     	comname = request.form['comname']
+    	# eresults = sql_query2(''' SELECT * FROM Flowers WHERE genus = ? AND species = ? ''',(genus,species))
+
     	sql_edit_insert(''' UPDATE Flowers set genus=?,species=?,comname=? WHERE genus=? and species=? and comname=?''', (genus,species,comname,old_genus,old_species, old_comname) )
     flowers_table = sql_query('''SELECT * FROM Flowers''')
+    sightings_results = sql_query3('''SELECT * FROM SIGHTINGS WHERE NAME='Draperia' ORDER BY SIGHTED DESC''', 10)
     return render_template('sqldatabase.html', 
+    	sightings_results=sightings_results,
     	flowers_table=flowers_table)
 
 # @app.route('/sightings', methods = ['POST', 'GET'])
